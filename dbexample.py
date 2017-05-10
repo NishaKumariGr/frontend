fe7fdc05d6a191f3c72e0af8ddd8bcec34ab9fe8
shanzae khan <shanzae92@gmail.com>
2017-05-10 19:07:56 -0400

    commiting changes in database, executing insert

--- dbexample.py
+++ dbexample.py
@@ -8,29 +8,46 @@ SERVER   = "sunapee.cs.dartmouth.edu"        # db server to connect to
 USERNAME = "f002bz6"                            # user to connect as
 PASSWORD = "quarpiz59."                            # user's password
 DATABASE = "f002bz6_db"                              # db to user
-QUERY    = "SELECT * FROM MANUSCRIPT;"       # query statement
+QUERY    = "SELECT * FROM AUTHOR;"       # query statement
 
 class command_Line_Interact(cmd.Cmd):
     """Command processor"""
     
     # 
     def do_register(self, line):
-        print ("original"+line)
-        print ('TOKENS:')
-        table, firstname,lastname,email, address = shlex.shlex(line)
+        #print ("original"+line)
+        #print ('TOKENS:')
+        table, firstname,lastname,email, address = shlex.split(line)
         print (table)
         print (firstname)
         print (lastname)
         print (email)
         print (address)
         Insert = "INSERT INTO `{0}` (`FirstName`,`LastName`,`MiddleName`,`EmailAddress`,`MailingAddress`, `Affiliation`) VALUES (\"{1}\",\"{2}\",NULL,\"{3}\",\"{4}\",NULL);".format(table,firstname,lastname,email,address)
+        print (Insert)
+        self.cursor.execute(Insert)
+        self.conn.commit()
+        self.cursor.execute(QUERY)
+        print("Query executed: '{0}'\n\nResults:".format(QUERY))
 
+        # print table header
+        print("".join(["{:<12}".format(col) for col in cursor.column_names]))
+        print("--------------------------------------------")
+
+        # iterate through results
+        for row in cursor:
+          print("".join(["{:<12}".format(col) for col in row]))
     
+    def do_exit(self):
+        self.con.close()
+        self.cursor.close()
+
     def do_EOF(self, line):
         return True
 
-    def extract_cursor(self, cur):
-      self.cursor=cur
+    def extract_cursor(self, cur, con):
+      self.cursor = cur
+      self.conn = con
 
 
 
@@ -59,6 +76,9 @@ if __name__ == "__main__":
       for row in cursor:
          print("".join(["{:<12}".format(col) for col in row]))
 
+      com_intr = command_Line_Interact()
+      com_intr.extract_cursor(cursor, con)
+      com_intr.cmdloop()
       # cleanup
       con.close()
       cursor.close()
@@ -68,7 +88,6 @@ if __name__ == "__main__":
     except:                                   # anything else
       print("Unexpected error: {0}".format(sys.exc_info()[0]))
    
-    command_Line_Interact().extract_cursor(cursor)
-    command_Line_Interact().cmdloop()
+    
       
     print("\nConnection terminated.", end='')
