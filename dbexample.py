a09c2f9098a858d26260003d06b7a0af76713e03
Nisha <nisha@saraswati.site>
2017-05-10 22:44:33 -0400

    ditinguishes insert based on table

--- dbexample.py
+++ dbexample.py
@@ -15,15 +15,27 @@ class command_Line_Interact(cmd.Cmd):
     
     # 
     def do_register(self, line):
-        #print ("original"+line)
-        #print ('TOKENS:')
-        table, firstname,lastname,email, address = shlex.split(line)
-        print (table)
-        print (firstname)
-        print (lastname)
-        print (email)
-        print (address)
-        Insert = "INSERT INTO `{0}` (`FirstName`,`LastName`,`MiddleName`,`EmailAddress`,`MailingAddress`, `Affiliation`) VALUES (\"{1}\",\"{2}\",NULL,\"{3}\",\"{4}\",NULL);".format(table,firstname,lastname,email,address)
+        tokens = shlex.split(line)
+        print (tokens[0])
+        #print (firstname)
+        #print (lastname)
+        #print (email)
+        #print (address)
+
+        if tokens[0] == "AUTHOR":
+          Insert = "INSERT INTO `{0}` (`FirstName`,`LastName`,`MiddleName`,`EmailAddress`,`MailingAddress`, `Affiliation`) VALUES (\"{1}\",\"{2}\",NULL,\"{3}\",\"{4}\",NULL);".format(tokens[0],tokens[1],tokens[2],tokens[3],tokens[4])
+        elif tokens[0] == "EDITOR":
+          Insert = "INSERT INTO `{0}` (`FirstName`,`LastName`,`MiddleName`) VALUES (\"{1}\",\"{2}\",NULL);".format(tokens[0],tokens[1],tokens[2])
+        elif tokens[0] == "REVIEWER":
+          lenOfList=len(tokens)
+          if lenOfList==3:
+            Insert = "INSERT INTO `{0}` (FirstName`,`EmailId`,`Affiliation`,`RICode1`,`RICode2`,`RICode3`,`LastName`,`MiddleName`) VALUES (\"{1}\",NULL,NULL,\"{2}\",NULL,NULL,\"{3}\",NULL);".format(tokens[0],tokens[1],tokens[3],tokens[2])
+          elif lenOfList==4:
+            Insert = "INSERT INTO `{0}` (FirstName`,`EmailId`,`Affiliation`,`RICode1`,`RICode2`,`RICode3`,`LastName`,`MiddleName`) VALUES (\"{1}\",NULL,NULL,\"{2}\",\"{3}\",NULL,\"{4}\",NULL);".format(tokens[0],tokens[1],tokens[3],tokens[4],tokens[2])
+          elif lenOfList==5:
+            Insert = "INSERT INTO `{0}` (FirstName`,`EmailId`,`Affiliation`,`RICode1`,`RICode2`,`RICode3`,`LastName`,`MiddleName`) VALUES (\"{1}\",NULL,NULL,\"{2}\",\"{3}\",\"{4}\",\"{5}\",NULL);".format(tokens[0],tokens[1],tokens[3],tokens[4],tokens[5],tokens[2])
+
+
         print (Insert)
         self.cursor.execute(Insert)
         self.con.commit()
@@ -50,8 +62,6 @@ class command_Line_Interact(cmd.Cmd):
     def extract_cursor(self, cur, con):
       self.cursor = cur
       self.con = con
-
-
 
 
 if __name__ == "__main__":
